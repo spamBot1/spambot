e6aa18559dca0792a193787b18f38eb2234020e0
spamBot1 <45138785+spamBot1@users.noreply.github.com>
2018-11-18 09:08:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "513625895550976011"; // ايدي السررفر
+var channel = "513626246618284047";//ايدي الروم
     setInterval(()=>{
     client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
     },305);
